b8d8607a53ecb6b419c4e46136be2e72fc094650
IamNotB0t <yinjue.lol@gmail.com>
2023-04-05 00:17:06 +0200

    second_commit

---
+++ Etch-A-Sketch/etchsketch.js
@@ -0,0 +1,12 @@
+const div = document.createElement('div');
+const grids = document.querySelector('.grids');
+
+
+//let canvasSize = prompt('what size?');
+for(let i =0 ; i<25; i++) {
+    grids.innerHTML += '<div class="box">hi</div>';
+
+}
+const box = document.querySelector('.box');
+
+
