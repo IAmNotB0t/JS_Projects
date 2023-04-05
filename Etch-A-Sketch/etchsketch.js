ca57fa2c6ab67f42769fe3c6d60885972dad833f
IamNotB0t <yinjue.lol@gmail.com>
2023-04-05 15:49:44 +0200

    canvas_size

--- Etch-A-Sketch/etchsketch.js
+++ Etch-A-Sketch/etchsketch.js
@@ -1,12 +1,40 @@
 const div = document.createElement('div');
 const grids = document.querySelector('.grids');
+const dimension = document.getElementById('dimension');
 
 
-//let canvasSize = prompt('what size?');
-for(let i =0 ; i<25; i++) {
-    grids.innerHTML += '<div class="box">hi</div>';
 
+dimension.addEventListener('change', handleUpdate);
+dimension.addEventListener('mousemove', handleUpdate);
+function handleUpdate() {
+    grids.innerHTML =''
+    const suffix = this.dataset.sizing;
+    document.documentElement.style.setProperty(`--${this.name}`, 100/this.value+suffix);
+    console.log(this.value);
+    
+    let canvasSize = this.value * this.value;
+    for(let i =0 ; i<canvasSize; i++) {
+        grids.innerHTML += '<div class="box"></div>';
+    };
+    
 }
-const box = document.querySelector('.box');
 
+
+
+
+
+
+
+const box = document.querySelectorAll('.box');
+
+box.forEach(grid =>{
+  
+    grid.addEventListener('mouseover', ()=> {
+        grid.style.backgroundColor ='black';
+    })
+    grid.addEventListener('drag', ()=>{
+        grid.style.backgroundColor = 'black';
+    })
+  
+});
 
