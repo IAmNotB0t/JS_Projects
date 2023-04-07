4677f8c11b574515c0ac593d06ee2fa8ba00df11
IamNotB0t <yinjue.lol@gmail.com>
2023-04-08 00:00:33 +0200

    draw_feature

--- Etch-A-Sketch/etchsketch.js
+++ Etch-A-Sketch/etchsketch.js
@@ -1,40 +1,39 @@
 const div = document.createElement('div');
 const grids = document.querySelector('.grids');
 const dimension = document.getElementById('dimension');
+const color = document.getElementById('color');
+
+color.addEventListener('change', colorOption);
 
 
 
 dimension.addEventListener('change', handleUpdate);
 dimension.addEventListener('mousemove', handleUpdate);
+
 function handleUpdate() {
     grids.innerHTML =''
     const suffix = this.dataset.sizing;
     document.documentElement.style.setProperty(`--${this.name}`, 100/this.value+suffix);
-    console.log(this.value);
     
+
     let canvasSize = this.value * this.value;
     for(let i =0 ; i<canvasSize; i++) {
         grids.innerHTML += '<div class="box"></div>';
     };
+
+    boxes = document.querySelectorAll('.box');
+    
+
+    boxes.forEach((pixels)=>{
+        pixels.addEventListener('mouseover', ()=> {
+            pixels.style.backgroundColor = 'var(--color)';
+        })
+    })
+
+};
+
+function colorOption() {
+    document.documentElement.style.setProperty(`--${this.name}`, this.value);
     
 }
-
-
-
-
-
-
-
-const box = document.querySelectorAll('.box');
-
-box.forEach(grid =>{
-  
-    grid.addEventListener('mouseover', ()=> {
-        grid.style.backgroundColor ='black';
-    })
-    grid.addEventListener('drag', ()=>{
-        grid.style.backgroundColor = 'black';
-    })
-  
-});
 
